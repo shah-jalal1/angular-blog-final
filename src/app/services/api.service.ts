d8e036c9d7ec15bf25606f0d52a9eaa086138e26
shahjalalcse <shah-jalal1>
2021-10-13 13:44:21 +0600

    ganeric done in api service

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,3 +1,4 @@
+import { PostModel } from './../interface/post-model';
 import { environment } from './../../environments/environment.prod';
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
@@ -13,34 +14,34 @@ export class ApiService {
   constructor(private http:HttpClient) { }
 
   postPost(data:any) {
-    return this.http.post<any>(API_BLOG+'add-blog', data)
+    return this.http.post<{success: boolean; data: PostModel[]; count: number}>(API_BLOG+'add-blog', data)
     .pipe(map((res:any)=>{
       return res;
     }))
     }
 
     getPost(){
-      return this.http.get<any>(API_BLOG+'get-all-blogs').pipe(map((res:any)=>{
+      return this.http.get<{success: boolean; data: PostModel[]; count: number}>(API_BLOG+'get-all-blogs').pipe(map((res:any)=>{
         return res;
       }));
     }
 
     getPostById(id : number) {
-      return this.http.get<any>(API_BLOG+'get-blog-by-blog-id/'+id).pipe(map((res:any)=>{
+      return this.http.get<{success: boolean; data: PostModel[]; count: number}>(API_BLOG+'get-blog-by-blog-id/'+id).pipe(map((res:any)=>{
         return res;
       }));
     }
 
 
     updatePost(data: any, id: number){
-      return this.http.put<any>(API_BLOG+'edit-blog-by-id/'+id, data).pipe(map((res:any)=>{
+      return this.http.put<{success: boolean; data: PostModel[]; count: number}>(API_BLOG+'edit-blog-by-id/'+id, data).pipe(map((res:any)=>{
         return res;
       }));
     }
 
 
     deletePost(id: number){
-      return this.http.delete<any>(API_BLOG+'delete-blog-by-id/'+id).pipe(map((res:any)=>{
+      return this.http.delete<{success: boolean; data: PostModel[]; count: number}>(API_BLOG+'delete-blog-by-id/'+id).pipe(map((res:any)=>{
         return res;
       }));
     }
